03f6f40b50bbbd61aca70b77cbd034083a857a6c
RalucaChiriac <120069921+RalucaChiriac@users.noreply.github.com>
2023-11-08 01:45:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -348,9 +348,6 @@ int main()
         std::cout<<std::endl<<std::endl<<std::endl;
     }
 
-
     std::cout<<std::endl<<"Va multumim pentru utilizarea programului nostru!"<< std::endl;
-
-
     return 0;
 }
